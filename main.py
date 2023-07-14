dcb360f376470ae7b47522cd3a5afe185465c949
kkamara <kamaracomputers@gmail.com>
2023-07-14 21:46:07 +0100

    Add project

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+'''
+https://www.youtube.com/watch?v=l8Imtec4ReQ
+Kivy Course - Create Python Games and Mobile Apps
+00:13:31
+'''
+class MainWidget(Widget):
+    pass
+
+class TheLabApp(App):
+    pass
+
+if __name__ == "__main__":
+    TheLabApp().run()
